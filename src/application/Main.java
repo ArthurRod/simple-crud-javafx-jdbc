40e26cc93e9424b37f5b5b370103cf1b76b552a8
Arthur Rodrigues <arthur9713@hotmail.com>
2024-04-23 15:09:44 -0300

    Função loadView adicionada ao Main View Controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,20 +8,29 @@ import javafx.stage.Stage;
 	
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
+			
 			scrollPane.setFitToWidth(true);
 			scrollPane.setFitToHeight(true);
-			Scene mainScene = new Scene(scrollPane);
+			
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Crud simples");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 	
 	public static void main(String[] args) {
