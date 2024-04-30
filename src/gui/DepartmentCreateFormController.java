37ea356b9db070e85a037127241e139a9e0c5807
Arthur Rodrigues <arthur9713@hotmail.com>
2024-04-30 15:49:29 -0300

    Validation exception implementada

--- src/gui/DepartmentCreateFormController.java
+++ src/gui/DepartmentCreateFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentCreateFormController implements Initializable {
@@ -66,6 +69,9 @@ public class DepartmentCreateFormController implements Initializable {
 			departmentService.saveOrUpdate(department);
 			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
+		} catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+
 		} catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
@@ -85,11 +91,19 @@ public class DepartmentCreateFormController implements Initializable {
 	}
 
 	private Department getFormData() {
+		ValidationException exception = new ValidationException("Validation Error");
 		Department department = new Department();
 
 		department.setId(Utils.tryParseToInt(txtId.getText()));
+
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Name can't be empty");
+		}
 		department.setName(txtName.getText());
 
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
 		return department;
 	}
 
@@ -104,10 +118,18 @@ public class DepartmentCreateFormController implements Initializable {
 	public void subscribeDataChangeListener(DataChangeListener listener) {
 		dataChangeListeners.add(listener);
 	}
-	
+
 	public void notifyDataChangeListeners() {
 		for (DataChangeListener listener : dataChangeListeners) {
 			listener.onDataChanged();
+		}
+	}
+
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+
+		if (fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
 		}
 	}
 }
