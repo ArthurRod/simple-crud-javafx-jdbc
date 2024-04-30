b98c618f745f2adcab0fd07a74c51f30fdc57bb2
Arthur Rodrigues <arthur9713@hotmail.com>
2024-04-30 13:48:40 -0300

    DepartmentCreateFormController criado

---
+++ src/gui/DepartmentCreateFormController.java
@@ -0,0 +1,49 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DepartmentCreateFormController implements Initializable{
+
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+		defineInputsLimits();
+	}
+	
+	private void defineInputsLimits() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	@FXML
+	public void onBtSaveAction() {
+		
+	}
+	
+	@FXML
+	public void onBtCancelAction() {
+		
+	}
+}
